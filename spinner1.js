4b69cd9f36e958418a5fa1bc2f556574254e1a5d
Ali Bas <alibas01@gmail.com>
2021-01-12 13:23:23 -0500

    spinning for some time

---
+++ spinner1.js
@@ -0,0 +1,94 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let delay = 100;
+let interval = 200;
+
+  setTimeout(() => {
+  
+    process.stdout.write('\r|   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, delay += interval);
+
+  setTimeout(() => {
+  
+    process.stdout.write('\r|   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, delay += interval);
+
+  setTimeout(() => {
+  
+    process.stdout.write('\r|   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, delay += interval);
+
+  setTimeout(() => {
+  
+    process.stdout.write('\r|   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, delay += interval);
+
+  setTimeout(() => {
+  
+    process.stdout.write('\r|   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay += interval);
+  
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, delay += interval);
+
